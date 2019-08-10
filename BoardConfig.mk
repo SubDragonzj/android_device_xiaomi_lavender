5cf60a9b92738ee77e882b749e21aa44d6da0e44
srfarias <erikofarias@gmail.com>
2019-08-10 01:46:36 -0300

    lavender: boardconfig: disable dexpreopt for prebuilts
    Signed-off-by: srfarias <erikofarias@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -280,6 +280,7 @@ ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
+            DONT_DEXPREOPT_PREBUILTS := true
       WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
     endif
   endif
