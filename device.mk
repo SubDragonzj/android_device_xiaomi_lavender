27e3773becdbb21f5397c27a2506d9c8ef928fc3
GuaiYiHu <guaiyihu@foxmail.com>
2019-09-13 10:48:00 +0800

    lavender: Disable ANT+
    Change-Id: Ie2147399406df02252e8529c37d10039b04f1590

--- device.mk
+++ device.mk
@@ -134,14 +134,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
-# ANT+
-PRODUCT_PACKAGES += \
-    AntHalService \
-    com.dsi.ant.antradio_library
-
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
-
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
