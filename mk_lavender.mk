260d5ddb605019da3d59655fb62d826eab944831
subdragonzj <zj502407679@gmail.com>
2019-09-15 00:08:39 +0800

    lavender: mk => mokee
    Change-Id: I8f48965d7636d1bc7b968b7b64a24e7f81fda582

--- mk_lavender.mk
+++ mk_lavender.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, device/xiaomi/lavender/device.mk)
 
 # Inherit some common Mokee stuff.
-$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+$(call inherit-product, vendor/mokee/config/common_full_phone.mk)
 
 PRODUCT_NAME := mk_lavender
 PRODUCT_BRAND := Xiaomi
