58e2e7f51f4f2fc35846694e18f9aadaddc10bc7
subdragonzj <zj502407679@gmail.com>
2019-11-14 19:51:17 +0800

    lavender: set device soong namespace

--- device.mk
+++ device.mk
@@ -34,6 +34,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
 
+# Soong
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
