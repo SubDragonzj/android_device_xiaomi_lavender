f5da259c5faa5a9ba2f239bd4bcd558386592603
subdragonzj <zj502407679@gmail.com>
2019-11-16 21:07:45 +0800

    lavender: disable Snap

--- device.mk
+++ device.mk
@@ -144,7 +144,6 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
-    Snap \
     libshim_camera
 
 # CNE
